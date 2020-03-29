ff75bb374b67e59cce0d9d6c367b7bf970743d57
dhiraj-patel <dhirajsuperhero@gmail.com>
2020-03-29 18:17:12 -0400

    restructuring

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for, request, redirect, session
-from utils import auth, tmdb, message
+from utils import auth, dispEvent, dispFood, databaseIO
 
 app = Flask(__name__)
+app.secret_key = 'nine'
 
-
-
-
-
+@app.route('/')
+@app.route('/login')
+def login():
+	if 'user' in session:
+		return redirect(url_for('home'))
+	return render_template('login.html')
